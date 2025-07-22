ce14b2b849226bd260adbb949d8fa9cecfc5f171
Onam Kwon <kon6443@gmail.com>
2025-07-22 17:49:53 +0900

    [next]UPDATE: oci next env CI/CD test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      hello next world, TEST:::: {process.env.NODE_ENV}, {process.env.TEST}
+      hello next world, TEST:::: {process.env.NODE_ENV},{" "}
+      {process.env.NEXT_PUBLIC_TEST}
     </div>
   );
 }
